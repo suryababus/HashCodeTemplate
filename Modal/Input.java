91f25f775d0a7a903341e2556948cdceb765b499
felix.ar <felix.ar@zohocorp.com>
2022-02-25 00:33:28 +0530

    Skill Vs Level & Contributor Map added

--- Modal/Input.java
+++ Modal/Input.java
@@ -1,6 +1,8 @@
 package Modal;
 
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Input {
@@ -8,6 +10,8 @@ public class Input {
     private String inputFileName;
     public List<Project> projects;
     public List<Contributor> contributors;
+
+    public HashMap<String, HashMap<Integer, List<Contributor>>> skillVsContributorMap;
 
     public Input(String inputFileName) {
         this.inputFileName = inputFileName;
@@ -18,5 +22,34 @@ public class Input {
 
     public void setInputFileName(String inputFileName) {
         this.inputFileName = inputFileName;
+    }
+
+    public static HashMap<String, HashMap<Integer, List<Contributor>>> getSkillVsContributorMap(List<Contributor> contributors)
+    {
+        HashMap<String, HashMap<Integer, List<Contributor>>> skillVsContributorsMap= new HashMap<>();
+
+        for(Contributor contributor : contributors)
+        {
+            for(String skill : contributor.getSkillMap().keySet())
+            {
+                skillVsContributorsMap.putIfAbsent(skill, new HashMap<Integer, List<Contributor>>());
+
+                int level = contributor.getSkillMap().get(skill);
+
+                HashMap<Integer, List<Contributor>> levelVsContributorMap = skillVsContributorsMap.get(skill);
+
+                levelVsContributorMap.putIfAbsent(level, new ArrayList<Contributor>());
+
+                List<Contributor> contributors_list_in_map = skillVsContributorsMap.get(skill).get(level);
+
+                contributors_list_in_map.add(contributor);
+
+                levelVsContributorMap.put(level, contributors_list_in_map);
+
+                skillVsContributorsMap.put(skill,levelVsContributorMap);
+            }
+        }
+
+        return skillVsContributorsMap;
     }
 }
