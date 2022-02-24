5a458d9c802f5711e08a3d7252222877ad68b2e6
ahinshapiro <ahinshapiro@gmail.com>
2022-02-25 01:32:20 +0530

    InputScript

--- Logic/InputScript.java
+++ Logic/InputScript.java
@@ -5,8 +5,14 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import Modal.Contributor;
 import Modal.Input;
+import Modal.Project;
 
 public class  InputScript {
 
@@ -27,7 +33,7 @@ public class  InputScript {
             StringBuffer sb = new StringBuffer();
             String str;
             while((str = reader.readLine())!= null){
-                sb.append(str);
+                sb.append(str).append("\n");
             }
             readInputs(fileName.substring(0, fileName.length()-4), sb.toString());
             reader.close();
@@ -49,15 +55,62 @@ public class  InputScript {
     LogicScript logicScript = new LogicScript();
     public void readInputs(String filename, String inputString){
         Input input = readLogic(inputString, filename);
+        HashMap<String, HashMap<Integer, List<Contributor>>> map = Input.getSkillVsContributorMap(input.contributors);
         logicScript.run(input);
     }
 
     private Input readLogic(String inputString, String filename){
         Input input = new Input(filename);
-        // Hashcode: read inputs
+        List<Contributor> contributors = new ArrayList<>();
+        List<Project> projects = new ArrayList<>();
 
+        String inputStringSplit[] = inputString.split("###");
+        String[] contributorValueArrayFull = inputStringSplit[1].split("\\r?\\n");
+        int i = 0;
+        while(i < contributorValueArrayFull.length){
+            String[] contributorValueArray = contributorValueArrayFull[i].split(" ");
+            if(contributorValueArray.length == 1) {
+                i++;
+                continue;
+            }
+            String name = contributorValueArray[0];
+            Integer skillCount = Integer.parseInt(contributorValueArray[1]);
+            Map<String, Integer> mp = new HashMap<>();
+            i++;
+            int j = i;
+            while(i < j + skillCount){
+                String[] skillValueArray = contributorValueArrayFull[i].split(" ");
+                mp.put(skillValueArray[0], Integer.parseInt(skillValueArray[1]));
+                i++;
+            }
+            contributors.add(new Contributor(name, skillCount, mp));
+        }
+        input.contributors = contributors;
 
-
+        i = 0;
+        String[] projectValueArrayFull = inputStringSplit[2].split("\\r?\\n");
+        while(i < projectValueArrayFull.length){
+            String[] projectValueArray = projectValueArrayFull[i].split(" ");
+            if(projectValueArray.length == 1) {
+                i++;
+                continue;
+            }
+            String projectName = projectValueArray[0];
+            Integer daysToCompleteProject = Integer.parseInt(projectValueArray[1]);
+            Integer score = Integer.parseInt(projectValueArray[2]);
+            Integer bestBefore = Integer.parseInt(projectValueArray[3]);
+            Integer skillsCount = Integer.parseInt(projectValueArray[4]);
+            i++;
+            int j = i;
+            Map<String, Integer> mp = new HashMap<>();
+            while(i < j + skillsCount){
+                String[] skillValueArray = projectValueArrayFull[i].split(" ");
+                mp.put(skillValueArray[0], Integer.parseInt(skillValueArray[1]));
+                i++;
+            }
+            projects.add(new Project(projectName,daysToCompleteProject,score, bestBefore, mp));
+        }
+        input.projects = projects;
         return input;
     }
 }
