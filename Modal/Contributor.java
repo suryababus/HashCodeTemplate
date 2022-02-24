4536093318a2636346f46fb308ff6590dff98b07
felix.ar <felix.ar@zohocorp.com>
2022-02-24 23:44:44 +0530

    Contributor Class Added

---
+++ Modal/Contributor.java
@@ -0,0 +1,11 @@
+package Modal;
+
+import java.util.HashMap;
+
+public class Contributor {
+	public String name;
+	public int noOfSkills;
+	public HashMap<String, Integer> skillMap; // Skill Vs Level
+
+	public Contributor(){}
+}
